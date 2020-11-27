793e611af05dc3ac7d52eac8bef76ac25c676857
Aizuddin Badry <aizuddinbadry@outlook.com>
2020-11-27 12:53:03 +0800

    added upload function to storage using active_storage

--- app/controllers/storages_controller.rb
+++ app/controllers/storages_controller.rb
@@ -1,2 +1,14 @@
 class StoragesController < ApplicationController
+
+    def create
+        @storage = Storage.new(storage_params)
+        if @storage.save
+            flash[:success] = 'Successful uploaded pdf.' #added notice after upload
+            redirect_to request.referrer
+        end
+    end
+
+    def storage_params
+        params.require(:storages).permit(:name, :pdf)
+    end
 end
--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,5 +1,3 @@
 class Storage < ApplicationRecord
     has_one_attached :pdf
-    has_one :pdf_attachment, dependent: :destroy
-    has_one :pdf_blob, through: :pdf_attachment
 end
