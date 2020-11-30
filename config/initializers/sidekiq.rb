bafe437b9764e5c3880d9a23d1e2c93af3ad3f01
Aizuddin Badry <aizuddinbadry@outlook.com>
2020-11-30 14:35:17 +0800

    updated sidekiq redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,3 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://redistogo:85e0fafd6a55897f7c2ea1d110c76337@scat.redistogo.com:10075/' }
+    config.redis = { url: 'redis://:fasttrade2020!@srv-captain--redis:6379/0' }
   end
